ba3cdb6922a4dd9b82717a69ae667e0f69c48cc9
Vaanemm <vladislav@andruetan.be>
2026-05-01 11:24:48 +0200

    file is nu csv, niet txt

--- StudentManagement/StudentManagement/database.cpp
+++ StudentManagement/StudentManagement/database.cpp
@@ -5,21 +5,19 @@
 #include <vector>
 
 std::string read() {
-	std::ifstream file("database.txt");
-	std::string content;
-	content = "nothing yet";
-	if (file.is_open())
-	{	
-		// We use a buffer here because it is faster and it lets us load everything in one go
-		std::stringstream buffer;
-		buffer << file.rdbuf(); // read entire file
-		content = buffer.str();
-	}
-	return content;
+	std::ifstream file("database.csv");
+	if (!file.is_open()) return "nothing yet";
+
+	std::string line;
+	std::getline(file, line); // skips the header
+
+	std::stringstream buffer;
+	buffer << file.rdbuf(); // read entire file
+	return buffer.str();
 }
 
 void write(const std::string& content) {
-	std::ofstream file("database.txt");
+	std::ofstream file("database.csv");
 	if (file.is_open())
 	{	
 		file << content;
@@ -27,8 +25,9 @@ void write(const std::string& content) {
 }
 
 void write2(const std::vector<std::shared_ptr<Student>>& stud_list) {
-	std::ofstream file("database.txt");
+	std::ofstream file("database.csv");
 	if (file.is_open()) {
+		file << "Surname, Name, E-mail, Password\n";
 		// we add const & in the for loop so the shared pointers doesnt increment its ammount of pointings
 		for (const auto& student : stud_list) {
 			file << student->PrepareForDatabase();
